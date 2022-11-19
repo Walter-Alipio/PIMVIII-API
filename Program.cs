f6a7c21789f975e2f47cdd0e4c0d1c8436857f90
Walter <silvawalteralipio@gmail.com>
2022-11-18 22:37:07 -0300

    inject TelefoneTipoService

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Cadastro_Teleatendimento.Services;
+using Cadastro_Teleatendimento.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+builder.Services.AddScoped<ITelefoneTipoService, TelefoneTipoService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,8 +18,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
