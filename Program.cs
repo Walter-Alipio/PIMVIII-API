68649d0fbe011a8bcc8018e4daa9ac99e255c304
Walter <silvawalteralipio@gmail.com>
2022-11-20 18:03:07 -0300

    add dependence injection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Cadastro_Teleatendimento.Data;
 using Cadastro_Teleatendimento.Services;
 using Cadastro_Teleatendimento.Services.Interfaces;
 
@@ -7,8 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-
+//dependency injection
 builder.Services.AddScoped<ITelefoneTipoService, TelefoneTipoService>();
+builder.Services.AddScoped<ITelefoneService, TelefoneService>();
+builder.Services.AddScoped<TipoDAO, TipoDAO>();
+builder.Services.AddScoped<TelefoneDAO, TelefoneDAO>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
