1b8ff3ed1fb0a094d74f6d3c73be02d5a5ad97f9
Walter <silvawalteralipio@gmail.com>
2022-11-25 17:48:57 -0300

    add automapper profile to telefone

---
+++ Profiles/TelefonePrifile.cs
@@ -0,0 +1,17 @@
+using System.Runtime.CompilerServices;
+using AutoMapper;
+using Cadastro_Teleatendimento.Data.DTOs.TelefoneDTO;
+using Cadastro_Teleatendimento.Models;
+
+namespace Cadastro_Teleatendimento.Profiles
+{
+  public class TelefoneProfile : Profile
+  {
+    public TelefoneProfile()
+    {
+      CreateMap<CreateTelefoneDto, Telefone>();
+
+      CreateMap<Telefone, ReadTelefoneDto>();
+    }
+  }
+}
