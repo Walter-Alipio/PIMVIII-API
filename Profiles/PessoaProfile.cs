fca19b14726cb16aea3379fed6b3f60e3c4903b3
Walter <silvawalteralipio@gmail.com>
2022-11-26 19:03:12 -0300

    add profile to automapper pessoa

---
+++ Profiles/PessoaProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using Cadastro_Teleatendimento.Data.DTOs.PessoaDTO;
+using Cadastro_Teleatendimento.Models;
+
+namespace Cadastro_Teleatendimento.Profiles
+{
+  public class PessoaProfile : Profile
+  {
+    public PessoaProfile()
+    {
+      CreateMap<CreatePessoaDto, Pessoa>()
+        .ForSourceMember(x => x.Telefones, opt => opt.DoNotValidate());
+      CreateMap<Pessoa, ReadPessoaDto>();
+    }
+  }
+}
